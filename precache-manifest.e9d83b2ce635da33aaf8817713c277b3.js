674885dbb87dbdc407c000bf65c3e3dde7616fc6
Kurt Mahaffey <kurtmahaf@hotmail.com>
2020-10-01 20:02:04 +1000

    Updates

---
+++ precache-manifest.e9d83b2ce635da33aaf8817713c277b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2556908bc51fbdb1ab26ef87e6f8fdb",
+    "url": "/collab/index.html"
+  },
+  {
+    "revision": "d2e7a7fa6469cca858a8",
+    "url": "/collab/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "830e1e1e913e9409c19f",
+    "url": "/collab/static/js/2.76c658cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/collab/static/js/2.76c658cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2e7a7fa6469cca858a8",
+    "url": "/collab/static/js/main.544c5242.chunk.js"
+  },
+  {
+    "revision": "be7679086cbdb727e1ec",
+    "url": "/collab/static/js/runtime-main.fe2c0636.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/collab/static/media/logo.5d5d9eef.svg"
+  }
+]);
